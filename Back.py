abea3511fb2e025765e1d505ab2c3b9008a2883c
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2022-06-28 14:37:52 -0300

    Adicionando automação parcial

--- Back.py
+++ Back.py
@@ -1,20 +1,25 @@
 import pymysql
 from Entrada import *
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
 #Criar conexão
 conexao = pymysql.connect(host='localhost',port=3306,database='gerenciador_senhas',
                           user='root',password='root@123',autocommit=True)
-#cursor
+#cursor1
 cursor = conexao.cursor()
 
-def criar_senha(descricao, password_user):
-    comando = ("INSERT INTO table_user (description, password) values (%s, %s)")  # INSERIR NA TABELA A PLATAFORMA E SUA SENHA
-    val = (descricao, password_user)  # criada tupla, para inserir dados na coluna
+def criar_plataforma(descricao, login_user, password_user):
+    comando = ("INSERT INTO table_user (description, login, password) values (%s, %s, %s)")  # INSERIR NA TABELA A PLATAFORMA E SUA SENHA
+    val = (descricao, login_user, password_user)  # criada tupla, para inserir dados na coluna
     cursor.execute(comando, val)  # execução
+
 
 def select():
     consulta = ("select * from table_user")  # selecionar a tabela, para poder visualiza-la
     cursor.execute(consulta)
-    linhas = cursor.fetchall()  # contar quantas linhas existem nas colunas
+    linhas = cursor.fetchall()  #contar quantas linhas existem nas colunas
     return linhas
 
 def quantidade():
@@ -25,9 +30,9 @@ def quantidade():
 
 def deletar(linha2,linhas,plataforma_desejada):
     for linha2 in linhas:
-        if plataforma_desejada == linha2[2]:  # Comparando, para quando o valor varrido for o desejado pelo usuário
+        if plataforma_desejada == linha2[3]:  # Comparando, para quando o valor varrido for o desejado pelo usuário
             excluir = ('DELETE FROM table_user WHERE id_description = (%s)')
-            valor_excluir = linha2[2]
+            valor_excluir = linha2[3]
             cursor.execute(excluir, valor_excluir)
 
 def reset():
--- Entrada.py
+++ Entrada.py
@@ -1,14 +1,10 @@
 from Back import *
 import pymysql
+from selenium import webdriver
 #Criar conexão
 conexao = pymysql.connect(host='localhost',port=3306,database='gerenciador_senhas',
                           user='root',password='root@123',autocommit=True)
 #cursor
-cursor = conexao.cursor()
-print('-' + '==-' * 8)
-print('  \033[1mGERENCIADOR DE SENHAS')
-desejo = 'n'
-
 while True:
     print('-' + '==-' * 8)
     menu = '''      MENU INICIAL
@@ -29,54 +25,72 @@ while True:
         #Caso o usuário tente fugir das possibilidades
             escolha = int(input("Digite uma opção válida: "))
         print()
+    #CRIAR NOVA SENHA
+    if escolha == 1:
+        descricao = str(input("Digite o nome da plataforma: ").upper())
+        login_user = str(input("Digite o login da plataforma: "))
+        password_user = str(input("Digite a senha: "))
 
-    #Dentro de um bloco Try, para tratar os erros
-    try:
-        #CRIAR NOVA SENHA
-        if escolha == 1:
-            descricao = str(input("Digite o nome da plataforma: ").upper())
-            password_user = str(input("Digite a senha: "))
-            criar_senha(descricao, password_user)
-            print('\n\033[;32mSeu novo cadastro obteve sucesso.\033[;1m\n') #Confirmação de cadastro
+        criar_plataforma(descricao, login_user, password_user)
+        print('\n\033[;32mSeu novo cadastro obteve sucesso.\033[;1m\n') #Confirmação de cadastro
 
-        #VISUALIZAR OU EXCLUIR SENHAS JÁ CADASTRADAS
-        if escolha ==2 or escolha == 3:
-            select()
-            if quantidade() < 1: #Se for menor que 1, é porque não possui nenhum registro no banco de dados
-                print()
-                print("\033[;31mVocê ainda não possui senhas! Volte ao menu para cadastrar!\033[;1m") #CASO NÃO TENHA SENHAS ELE NÃO CONTINUA
-                print()
+    #VISUALIZAR OU EXCLUIR SENHAS JÁ CADASTRADAS
+    if escolha ==2 or escolha == 3:
+        select()
+        if quantidade() < 1: #Se for menor que 1, é porque não possui nenhum registro no banco de dados
+            print()
+            print("\033[;31mVocê ainda não possui senhas! Volte ao menu para cadastrar!\033[;1m") #CASO NÃO TENHA SENHAS ELE NÃO CONTINUA
+            print()
+        else:
+            print("\nPLATAFORMAS DISPONÍVEIS:\n") #Exibição das possibilidades
+            for linha in select():
+                print(f"[{linha[3]}]", linha[0])
+            print()
+            consulta = ("select * from table_user")
+            cursor.execute(consulta)
+            linhas = cursor.fetchall()
+            if escolha == 2:
+                plataforma_desejada = int(input("Qual você quer acessar? "))
+                for linha2 in select():
+                    if plataforma_desejada == linha2[3]: #Comparando, para quando o valor varrido for o desejado pelo usuário, exibir após o if
+                        print()
+                        print("Plataforma:", linha2[0])
+                        print("Login:", linha2[1])
+                        print("Senha: *******")
+                        #2print("Senha:", linha2[1])
+                        print()
+                        deseja_acessar = input('Deseja acessar automaticamente à plataforma? [s/n] ').lower()
+                        if deseja_acessar == 's': #automação
+                            driver = webdriver.Chrome(executable_path=r"C:\Users\jenni\chromedriver\chromedriver")
+                            try:
+                                driver.get(f'https://www.{linha2[0]}.com.br')
+                                campo_login = driver.find_element(By.CSS_SELECTOR, "#email")
+                                campo_login.send_keys(linha2[1])
+
+                                campo_password = driver.find_element(By.CSS_SELECTOR, '#pass')
+                                campo_password.send_keys(linha2[2] + Keys.RETURN)
+                            except:
+                                driver.find_element(By.XPATH,'//*[@id="details-button"]').click()
+                                driver.find_element(By.XPATH,'//*[@id="proceed-link"]').click()
+                                driver.get(f'https://www.{linha2[0]}.com.br')
+                                campo_login = driver.find_element(By.CSS_SELECTOR, "#email")
+                                campo_login.send_keys(linha2[1])
+                                campo_password = driver.find_element(By.CSS_SELECTOR, '#pass')
+                                campo_password.send_keys(linha2[2] + Keys.RETURN)
+                        else:
+                            pass
             else:
-                print("\nPLATAFORMAS DISPONÍVEIS:\n") #Exibição das possibilidades
-                for linha in select():
-                    print(f"[{linha[2]}]", linha[0])
-                print()
-                consulta = ("select * from table_user")
-                cursor.execute(consulta)
-                linhas = cursor.fetchall()
-                if escolha == 2:
-                    plataforma_desejada = int(input("Qual você quer acessar? "))
+                plataforma_desejada = int(input("Qual você quer deletar? "))
+                confirmacao = str(input("\033[;31mRealmente deseja deletar o registro? A ação será permanente: [s/n] \033[;1m").lower())
+                if confirmacao == "s":
                     for linha2 in select():
-                        if plataforma_desejada == linha2[2]: #Comparando, para quando o valor varrido for o desejado pelo usuário, exibir após o if
-                            print()
-                            print("Plataforma:", linha2[0])
-                            print("Senha:", linha2[1])
-                            print()
-                else:
-                    plataforma_desejada = int(input("Qual você quer deletar? "))
-                    confirmacao = str(input("\033[;31mRealmente deseja deletar o registro? A ação será permanente: [s/n] \033[;1m").lower())
-                    if confirmacao == "s":
-                        for linha2 in select():
-                            if plataforma_desejada == linha2[2]: #Comparando, para quando o valor varrido for o desejado pelo usuário
-                                deletar(linha2, linhas, plataforma_desejada)
-                                print('\n\033[;32mDeletado com sucesso!\033[;1m\n')
-                                reset()
-        elif escolha == 4:
-            print('\n\033[;32mAté breve!') #Encerrar programa
-            break
-
-    except Exception as e:
-        print(f"Erro: {e}")
+                        if plataforma_desejada == linha2[3]: #Comparando, para quando o valor varrido for o desejado pelo usuário
+                            deletar(linha2, linhas, plataforma_desejada)
+                            print('\n\033[;32mDeletado com sucesso!\033[;1m\n')
+                            reset()
+    elif escolha == 4:
+        print('\n\033[;32mAté breve!') #Encerrar programa
+        break
     desejo = str(input("Deseja voltar ao menu inicial? [s/n] ").lower())
     print()
 
